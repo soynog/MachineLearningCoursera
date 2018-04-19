89050fdcf35dcabfc88e998208542239d07931dc
soynog <nicolas.o.garcia@gmail.com>
2018-04-19 14:29:44 -0400

    Exercise 2

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,9 +20,10 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+hypo = sigmoid(X * theta);
 
-
-
+J = (1 / m) * ( (-1 .* y' * log( hypo ) - (1 .- y') * log(1 .- hypo) ) );
+grad = (1 / m) * (transpose(hypo - y) * X);
 
 
 
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -17,7 +17,9 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
+hypo = sigmoid(X * theta);
+J = (1 / m) * ( (-1 .* y' * log( hypo ) - (1 .- y') * log(1 .- hypo) ) ) + (lambda / (2 * m)) * sum(theta(2:end) .^ 2) ;
+grad = (1 / m) * (transpose(hypo - y) * X) + transpose([0;(lambda / m) .* theta(2:end)]);
 
 
 
